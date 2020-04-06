2a91fcd5e1c947fc299a00bcc1ea01f14c63d752
Shisan Nafiz <34602595+shisannafiz@users.noreply.github.com>
2020-04-06 15:43:33 -0400

    fixed format

--- src/OSProcess.java
+++ src/OSProcess.java
@@ -6,8 +6,8 @@ public class OSProcess {
 	
 	public OSProcess() {};
 	
-	public void start(ProcessBuilder pb) throws IOException 
-	{
+	public void start(ProcessBuilder pb) throws IOException {
+		
 		Process pro = pb.start(); 
 		InputStream is = pro.getInputStream();     
 		InputStreamReader isr = new InputStreamReader(is);     
@@ -19,43 +19,33 @@ public class OSProcess {
 		br.close(); 
 	}
 	
-	public void setHome(ProcessBuilder pb) 
-	{
+	public void setHome(ProcessBuilder pb) {
 		File home = new File(System.getProperty("user.dir"));
 		pb.directory(home);
 	}
 	
-	public void changeDir(ProcessBuilder pb, List<String> list) 
-	{
+	public void changeDir(ProcessBuilder pb, List<String> list) {
 		String nextDir = list.get(1);
 		File currDir = pb.directory();
 		
 		File path = new File(currDir + "\\" + nextDir);
-		if(path.exists())
-		{
+		if(path.exists()) {
 			pb.directory(path);
-		} 
-		else 
-		{
+		} else {
 			System.out.println("Please input a valid directory!");
 		}
 	}
 	
-	public void printHistory(List<String> hist) 
-	{
-		for(int i = 0; i < hist.size()-1; i++) 
-		{
+	public void printHistory(List<String> hist) {
+		for(int i = 0; i < hist.size()-1; i++) {
 			System.out.println(i + " " + hist.get(i));
 		}
 	}
 	
-	public void doPrevious(ProcessBuilder pb, List<String> hist) 
-	{
+	public void doPrevious(ProcessBuilder pb, List<String> hist) {
 		if(hist.size() == 1) {
 			System.out.println("No previous command!");
-		}
-		else
-		{
+		} else {
 			List<String> list = new ArrayList<String>();
 			list.add(hist.get(hist.size()-2));
 			pb.command(list);
@@ -63,8 +53,7 @@ public class OSProcess {
 		}
 	}
 	
-	public void doCommand(ProcessBuilder pb, List<String> cmd, List<String> hist) 
-	{
+	public void doCommand(ProcessBuilder pb, List<String> cmd, List<String> hist) {
 		int num = Character.getNumericValue(cmd.get(0).charAt(1));
 		List<String> list = new ArrayList<String>();
 		list.add(hist.get(num));
